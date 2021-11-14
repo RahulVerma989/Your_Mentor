b0618b579d3987767d9d2effa26004356f61aaed
RahulVerma989 <48805821+RahulVerma989@users.noreply.github.com>
2021-11-14 15:47:23 +0530

    removed all the files

--- Your_Mentor/app.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config({ path: '../private/config.env'});
-require('./dbcreate.js');
-const SendEmail = require('./email.js');
-
-SendEmail('rv9891211164@gmail.com','Your Mentor first Test mail 🙂','text','Hi!, good night 🥱😴 and sweet dreams from Your Mentor.');
-
