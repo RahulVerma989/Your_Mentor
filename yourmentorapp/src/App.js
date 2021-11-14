1f78d1d5eeb2140eb92ab6990dd98dcda46ea6f1
RahulVerma989 <48805821+RahulVerma989@users.noreply.github.com>
2021-11-14 16:14:53 +0530

    added app folder

---
+++ yourmentorapp/src/App.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+class App extends React.Component {
+  state = {
+    name: "JOHN DOE"
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:3000")
+      .then(res => res.json())
+      .then(data => this.setState({ name: data.name }))
+  }
+
+  render() {
+    return (
+      <h1>Hello {this.state.name}!</h1>
+    )
+  }
+}
+
+export default App;
