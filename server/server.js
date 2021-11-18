f3c4dd39c25535597b0aaab4d9ab3b639a70114d
RahulVerma989 <48805821+RahulVerma989@users.noreply.github.com>
2021-11-18 12:51:42 +0530

    new file system creted

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const app = express()
+const router = express.Router();
+
+app.get("/", function(req, res) {
+    res.send("It's working!")
+  })
+
+// login page
+router.get('/login',(req,res)=>res.send('Login'));
+
+app.listen(3001, () => {
+  console.log("app listening on port 3001")
+})
+// app.get("/", function(req, res) {
+//   res.send({"name": "Jane Doe"}) // Should be json format
+// })
+
+module.exports = router;
